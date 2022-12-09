8e260fb819ad1141106b0c5d02f09d1d110ca549
aneshpugal <47292153+aneshpugal@users.noreply.github.com>
2022-12-09 12:39:08 +0530

    Update log_sender.py
    debug

--- EventHubs_Logs/log_sender.py
+++ EventHubs_Logs/log_sender.py
@@ -180,7 +180,8 @@ def main(eventMessages: func.EventHubEvent):
                 log_category = (log_events[0]['category' if 'category' in log_events[0] else 'Category']).replace('-', '_')
                 print("log_category" + " : "+ log_category)
                 log_category = 'S247_'+log_category
-
+            else:
+                print("Events:",log_events[0])
             if log_category in os.environ:
                 print("log_category found in input arguments")
                 logtype_config = json.loads(b64decode(os.environ[log_category]).decode('utf-8'))
